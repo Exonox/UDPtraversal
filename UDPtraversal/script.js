694af0476563c435ef128da07def3ee0c5b1939c
Jaedyn Theobald <105262741+Exonox@users.noreply.github.com>
2025-03-14 01:02:24 +1300

    added all the code
    regarding peer to peer video connection

--- UDPtraversal/script.js
+++ UDPtraversal/script.js
@@ -96,7 +96,57 @@ document.addEventListener('DOMContentLoaded', (event) => {
         updateIcon(); // Update the icon on toggle
     });
 });
+//space for adding code to host the video
+const hash = location.hash;
+const id = hash.split('#')[1]
+console.log(id);
 
+// var peer = new peer(id,
+//     host:'/',
+//     port:'3001',
+
+// );
+
+var conn = peer.connect('another-peers-id');
+// on open will be launch when you successfully connect to PeerServer
+conn.on('open', function(){
+  // here you have conn.id
+  conn.send('hi!');
+});
+
+peer.on('connection', function(conn) {
+    conn.on('data', function(data){
+      // Will print 'hi!'
+      console.log(data);
+    });
+  });
+
+  var getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+getUserMedia({video: true, audio: true}, function(stream) {
+  var call = peer.call('another-peers-id', stream);
+  call.on('stream', function(remoteStream) {
+    // Show stream in some video/canvas element.
+  });
+}, function(err) {
+  console.log('Failed to get local stream' ,err);
+});
+
+
+//answer
+var getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+peer.on('call', function(call) {
+  getUserMedia({video: true, audio: true}, function(stream) {
+    call.answer(stream); // Answer the call with an A/V stream.
+    call.on('stream', function(remoteStream) {
+      // Show stream in some video/canvas element.
+    });
+  }, function(err) {
+    console.log('Failed to get local stream' ,err);
+  });
+});
+
+
+//call
 // Function to check whether an element is in the viewport
 function isInViewport(element) {
     const rect = element.getBoundingClientRect();
